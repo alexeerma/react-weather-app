dd0d60970aa618624434f0774d821aed5cf03fbc
alexeerma <aleksander.eerma@hotmail.com>
2023-04-04 19:05:14 +0300

    Added the styling, although via Css not tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -30,30 +30,30 @@ function App() {
           placeholder='Enter Location'
           type="text" />
       </div>
-      <div className="">
-        <div className="">
-          <div className="">
+      <div className="container">
+        <div className="top">
+          <div className="location">
             <p>{data.name}</p>
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="">
+          <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {data.name !== undefined &&
-          <div className="">
-            <div className="">
+          <div className="bottom">
+            <div className="feels">
               {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
               <p>Feels Like</p>
             </div>
-            <div className="">
+            <div className="humidity">
               {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className="">
+            <div className="wind">
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
               <p>Wind Speed</p>
             </div>
